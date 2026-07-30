7e36d38c54b6f708c5296ffe008ed400340d393c
Alexander Lais <alexander.lais@sap.com>
2026-07-30 13:21:13 +0200

    fix(sample): make tools.go a tool so it imports `ironcore` (#383)
    Without the import, the CI for the sample fails.
    
    Signed-off-by: Alexander Lais <alexander.lais@sap.com>

--- sample/tools.go
+++ sample/tools.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build ignore
+//go:build tools
 
 package sample
 
